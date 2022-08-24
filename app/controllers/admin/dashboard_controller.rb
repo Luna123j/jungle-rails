3d4248f23a3bd59e1443013f5b974e8595eabe13
Shenglun Jin <sunny.jsl.7@gmail.com>
2022-08-24 23:38:27 +0000

    edit admin dashboard page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['AUTH_USERNAME'] , password: ENV['AUTH_PASSWORD']
 
   def show
+    @total_products = Product.count
+    @total_category = Category.count
   end
 end
