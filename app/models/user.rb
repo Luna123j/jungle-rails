8125c32ba93f6e2d231d90b11e752f06bf1d9b6e
Shenglun Jin <sunny.jsl.7@gmail.com>
2022-08-30 01:17:40 +0000

    test valid info to sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ApplicationRecord
   has_secure_password
+  before_save :downcase_email
+  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}, presence: true, uniqueness: true
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :password, length: { minimum: 3 }, on: :create
+
+  def self.authenticate_with_credentials(email,password)
+    user = User.find_by_email(email.downcase)
+    user.authenticate(password)
+  end
+
+  private
+  def downcase_email
+    self.email = email.downcase
+  end
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,131 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    it "should include a password field and a password confirmation field" do
+        @user = User.new(
+          firstname: "Shenglun",
+          lastname: "Jin",
+          email:"test@test.com",
+          password: "123",
+          password_confirmation: "123"
+        )
+        @user.save!
+        expect(@user.id).to be_present
+
+    end
+
+    it "should return error if the two filed do not have identical password" do
+      @user = User.new(
+        firstname: "Shenglun",
+        lastname: "Jin",
+        email:"test@test.com",
+        password: "123",
+        password_confirmation: "321"
+      ) 
+      @user.validate
+      expect( @user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it "should only allow unique email " do
+      @user1 = User.new(
+        firstname: "Shenglun",
+        lastname: "Jin",
+        email:"test@test.com",
+        password: "123",
+        password_confirmation: "123"
+      )
+      @user1.save!
+      @user2 = User.new(
+        firstname: "Shenglun",
+        lastname: "Jin",
+        email:"test@test.com",
+        password: "123",
+        password_confirmation: "123"
+      )  
+      @user2.validate
+
+      expect( @user2.errors.full_messages).to include("Email has already been taken")
+
+    end
+    
+    it "should create user with validate email" do
+      @user = User.new(
+        firstname: "Shenglun",
+        lastname: "Jin",
+        password: "123",
+        password_confirmation: "321"
+      ) 
+      @user.validate
+      expect( @user.errors.full_messages).to include("Email can't be blank")
+    end
+
+    it "should create user with validate firstname" do
+      @user = User.new(
+        lastname: "Jin",
+        email:"test@test.com",
+        password: "123",
+        password_confirmation: "321"
+      ) 
+      @user.validate
+      expect( @user.errors.full_messages).to include("Firstname can't be blank")
+    end
+
+    it "should create user with validate lastname" do
+      @user = User.new(
+        firstname: "Shenglun",
+        email:"test@test.com",
+        password: "123",
+        password_confirmation: "321"
+      ) 
+      @user.validate
+      expect( @user.errors.full_messages).to include("Lastname can't be blank")
+    end
+
+    it "should return error if password length less than 3" do
+      @user = User.new(
+        firstname: "Shenglun",
+        lastname: "Jin",
+        email:"test@test.com",
+        password: "12",
+        password_confirmation: "12"
+      ) 
+      @user.validate
+      expect( @user.errors.full_messages).to include("Password is too short (minimum is 3 characters)")
+    end
+  end
+
+  describe '.authenticate_with_credentials' do
+    User.new(
+      firstname: "Shenglun",
+      lastname: "Jin",
+      email:"test@test.com",
+      password: "123",
+      password_confirmation: "123"
+    )    
+    it "should return a valid user if pass authentication" do
+      user=User.authenticate_with_credentials("test@test.com","123")
+      
+      expect(user.id).to be_present
+    end
+   
+
+  end
+
+  describe 'edge case' do
+    User.new(
+      firstname: "Shenglun",
+      lastname: "Jin",
+      email:"test@test.com",
+      password: "123",
+      password_confirmation: "123"
+    )    
+    it "should return a valid user if pass authentication" do
+      user=User.authenticate_with_credentials("test@test.com","123")
+      
+      expect(user.id).to be_present
+    end
+   
+
+  end
+end
