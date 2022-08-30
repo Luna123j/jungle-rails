fd8fe05eb9e1e105d22fac663c4f312306924600
Shenglun Jin <sunny.jsl.7@gmail.com>
2022-08-30 21:17:49 +0000

    add test for product details

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-describe('Jungle test home page', () => {
+describe('Jungle test product details', () => {
   beforeEach(() => {
     cy.visit('/')
   })
@@ -11,6 +11,15 @@ describe('Jungle test home page', () => {
 
   it("There is 2 products on the page", () => {
     cy.get(".products article").should("have.length", 2);
+  });
+  
+  it("should navigate to product details page after click on an product", () => {
+    cy.get(".products article")
+      .first()
+      .click()
+    cy.contains("Scented Blade")
+
+
   });
   
 })
