4af3ba344d0c0b2679192206d0b8145a05dff63f
Shenglun Jin <sunny.jsl.7@gmail.com>
2022-08-30 19:51:25 +0000

    add cypress test for home page

--- cypress.config.js
+++
@@ -1,7 +0,0 @@
-const { defineConfig } = require('cypress')
-
-module.exports = defineConfig({
-  screenshotsFolder: 'tmp/cypress_screenshots',
-  trashAssetsBeforeRuns: false,
-  videosFolder: 'tmp/cypress_videos',
-})
